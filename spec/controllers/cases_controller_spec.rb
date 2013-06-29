f0a068cfa85309ce227de8fd08e894ddc9351aaf
Adam Wead <amsterdamos@gmail.com>
2013-06-29 16:54:19 -0400

    Finishing controller test coverage

--- spec/controllers/cases_controller_spec.rb
+++ spec/controllers/cases_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe CasesController do
 
+  fixtures :cases
+
   it "should render a new case page" do
     get :new
     response.should be_success
@@ -15,6 +17,21 @@ describe CasesController do
   it "should list all the cases" do
     get :index
     response.should be_success
+  end
+
+  it "should update a case" do
+    put :update, :id => "1", :case => {name: "new name", path: "new path"}
+    response.should be_redirect
+  end
+
+  it "should edit a case" do
+    get :edit, :id => "1"
+    response.should be_success
+  end
+
+  it "should delete a case" do
+    delete :destroy, :id => "1"
+    response.should be_redirect
   end
 
 end
