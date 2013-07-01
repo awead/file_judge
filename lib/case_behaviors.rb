5aeb2c656521011a25315928b25161bb7741ac1f
Adam Wead <amsterdamos@gmail.com>
2013-07-01 10:57:07 -0400

    other way around...

--- lib/case_behaviors.rb
+++ lib/case_behaviors.rb
@@ -1,7 +1,7 @@
 module CaseBehaviors
 
-  def build
-    new_case = File.join(FileJudge::Application.config.basepath, self.path)
+  def build_case path
+    new_case = File.join(FileJudge::Application.config.basepath, path)
     if File.directory?(new_case)
       return new_case
     else
--- spec/lib/case_behaviors_spec.rb
+++ spec/lib/case_behaviors_spec.rb
@@ -4,7 +4,6 @@ describe CaseBehaviors do
 
   before :each do
     class TestCase
-      attr_accessor :path
       include CaseBehaviors
     end
     @test = TestCase.new 
@@ -13,15 +12,12 @@ describe CaseBehaviors do
   describe "building a case" do
     
     it "should return the path to a case" do
-      @test.path = "good"
-      @test.build.should == "spec/fixtures/good"
-      @test.path = "bad"
-      @test.build.should  == "spec/fixtures/bad"
+      @test.build_case("good").should == "spec/fixtures/good"
+      @test.build_case("bad")  == "spec/fixtures/bad"
     end
 
     it "should return false when the case isn't found" do
-      @test.path = "foo"
-      @test.build.should be_false
+      @test.build_case("foo").should be_false
     end
   end
 
